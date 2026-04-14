421001edf79254208286795cf3f5c516ce52041f
Daksh Ahuja <ahuja@DellG15.localdomain>
2026-04-14 16:17:06 +0530

    RSA

--- backend/db.js
+++ backend/db.js
@@ -6,10 +6,16 @@ const pool = mariadb.createPool({
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
     port: process.env.MYSQLPORT,
-    connectionLimit: 25,
+    connectionLimit: 20,
     acquireTimeout: 20000,
     waitForConnections: true,
-    queueLimit: 0
+    queueLimit: 0,
+
+    // 🔥 IMPORTANT FIXES
+    ssl: {
+        rejectUnauthorized: false
+    },
+    allowPublicKeyRetrieval: true
 });
 
 module.exports = pool;
